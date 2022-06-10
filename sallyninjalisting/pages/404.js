65d9b5a74b168bc8bb144a634be95bb97d044241
Sally Resch <sallyresch27@gmail.com>
2022-06-10 10:44:26 +0200

    added useEffect

--- sallyninjalisting/pages/404.js
+++ sallyninjalisting/pages/404.js
@@ -1,5 +1,16 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 const FourOfour = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      //router.go(-1)
+      router.push('/');
+    }, 3000)
+  }, [])
   return (
     <div className="not-found">
       <h1>Oooooopsies......</h1>
